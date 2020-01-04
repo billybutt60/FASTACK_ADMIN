ae4cbb11986fa2ac5b920ec7a06d28e7548582c6
billy butt <billybutt60@gmail.com>
2020-01-04 19:08:24 +0500

    Merge branch 'master' of C:\Users\Bilal\AndroidStudioProjects\FASTACK_ADMIN with conflicts.

---
+++ app/src/main/java/com/yarmadadgar/fastack_admin/Home.java
@@ -0,0 +1,15 @@
+package com.yarmadadgar.fastack_admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
--- app/src/main/java/com/yarmadadgar/fastack_admin/MainActivity.java
+++ app/src/main/java/com/yarmadadgar/fastack_admin/MainActivity.java
@@ -1,15 +1,72 @@
 package com.yarmadadgar.fastack_admin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private TextInputEditText email, pass;
+    private Button signInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hey zahoor");
+        mAuth = FirebaseAuth.getInstance();
+
+        Init();
+
+    }
+    private void Init(){
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        signInBtn = findViewById(R.id.signInBtn);
+
+    }
+    private void signIn(String email, String password){
+        if(!email.isEmpty() && !password.isEmpty()) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Intent it = new Intent(MainActivity.this, Home.class);
+                                startActivity(it);
+                                finish();
+
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(MainActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
+
+
+                        }
+                    });
+        }
+    }
+
+    public void signIn(View view) {
+        String email_s = email.getText().toString();
+        String pass_s = pass.getText().toString();
+        signIn(email_s,pass_s);
     }
 }
